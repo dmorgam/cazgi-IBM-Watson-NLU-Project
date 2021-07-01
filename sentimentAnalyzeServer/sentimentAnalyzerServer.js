2061092fd8a3214e56519ad5a4aab3d1ce101d7e
David Moreno <dmorgam94@gmail.com>
2021-07-01 17:31:05 -0400

    nlu service conection definition

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -1,4 +1,25 @@
 const express = require('express');
+const dotenv = require('dotenv')
+dotenv.config()
+
+const getNLUInstance = () => {
+  const api_key = process.env.API_KEY
+  const api_url = process.env.API_URL
+
+  const NaturalLanguajeUnderstandingV1 = require('ibm-watson/natural-languaje-understanding/v1')
+  const { IamAuthenticator } = require('ibm-watson/auth')
+
+  const NatLangUnd = new NaturalLanguajeUnderstandingV1({
+    version: '2020-08-01',
+    autenticator: new IamAuthenticator ({
+      apikey: api_key
+    }),
+    serviceUrl: api_url
+  })
+
+  return NatLangUnd
+}
+
 const app = new express();
 
 app.use(express.static('client'))
